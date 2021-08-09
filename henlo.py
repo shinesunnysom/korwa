d5eea5dc0cc79ca5bebe24d405627a48b0aed8d2
Shine-Sunny Som <16434266+shinesunnysom@users.noreply.github.com>
2021-08-09 15:10:23 -0700

    Update henlo.py

--- henlo.py
+++ henlo.py
@@ -1,11 +1,10 @@
  # This program says hello and asks for my name.
-
 print('Hello, world!')
 print('What is your name?')    # ask for their name
 myName = input()
 print('Nice to meet you,' + myName)
 print('length of you name is:')
 print(len(myName))
-print('What is your age>') # ask for age number
+print('What is your age?')    # ask for their age
 myAge = input()
-print('You will be ' +str(int(myAge) + 1) + ' in a year.')
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
