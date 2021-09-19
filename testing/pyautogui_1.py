9b0a55f933c7d57fe5458839a6a5978ff8c2078c
Shine-Sunny Som <16434266+shinesunnysom@users.noreply.github.com>
2021-09-18 17:43:08 -0700

    Create pyautogui_1.py

---
+++ testing/pyautogui_1.py
@@ -0,0 +1,6 @@
+import pyautgui, time
+time.sleep(5) # wait 5 secs 
+pyautogui.click() # focus in front of screen
+distance = 400 # abitrary value, 400px
+  while distance > 0:
+      pyautogui.dragReli(distance, 0, duration
