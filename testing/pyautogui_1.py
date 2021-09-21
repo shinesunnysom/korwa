157fc49ecc204c012a9dd4cfdca7bb4911655e8c
Shine-Sunny Som <16434266+shinesunnysom@users.noreply.github.com>
2021-09-20 17:15:18 -0700

    Update pyautogui_1.py

--- testing/pyautogui_1.py
+++ testing/pyautogui_1.py
@@ -3,6 +3,6 @@ time.sleep(5) # wait 5 secs
 pyautogui.click() # focus in front of screen
 distance = 400 # abitrary value, 400px
 while distance > 0:
-    pyautogui.dragReli(distance, 0, duration=0.2)
+    pyautogui.dragRel(distance, 0, duration=0.2)
     distance = distance - 50
     pyautgui.dragRel(0, distance, duration=0.2)
