368423d85d66fcd713bf192e50272edd43e04f88
Shine-Sunny Som <16434266+shinesunnysom@users.noreply.github.com>
2021-09-23 13:46:56 -0700

    Update pyautogui_1.py

--- testing/pyautogui_1.py
+++ testing/pyautogui_1.py
@@ -5,4 +5,7 @@ distance = 400 # abitrary value, 400px
 while distance > 0:
     pyautogui.dragRel(distance, 0, duration=0.2)
     distance = distance - 50
-    pyautgui.dragRel(0, distance, duration=0.2)
+    pyautogui.dragRel(0, distance, duration=0.2)
+    pyautogui.dragRel(-distance, 0, duration=0.2)
+    distance = distance - 50
+    distance.dragRel(0, -distance, duration=0.2)  
