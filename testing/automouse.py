db4778c1086ef24facf5499f9c41eafc39955200
Shine-Sunny Som <16434266+shinesunnysom@users.noreply.github.com>
2021-11-10 10:49:45 -0800

    Create automouse.py

---
+++ testing/automouse.py
@@ -0,0 +1,12 @@
+import pyautogui
+
+#Create failsafe, leave mouse upper left corner to halt
+pyautogui.FAILSAFE = True
+
+#pseudo code: x and y determines your resolution
+#print(pyautogui.size()) by obtaining coordinates
+#
+#create mouse to move a specific location
+#eg. pyautogui.motTo(100, 100, duration=0.25)
+
+#code here
